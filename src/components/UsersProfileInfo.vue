7757e304447cadafef6bdf6526e43c4d4d9552cf
Zack Borges <zackartborges@gmail.com>
2021-07-15 14:58:01 -0400

    deletes duplicates

--- src/components/UsersProfileInfo.vue
+++ src/components/UsersProfileInfo.vue
@@ -3,37 +3,29 @@
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   data: function () {
     return {
-      user: {},
-      errors: [],
+      // -
     };
   },
   methods: {
-    // submit: function () {
-    //   var params = {
-    //     name: this.name,
-    //     email: this.email,
-    //     username: this.username,
-    //     image_url: this.imageUrl,
-    //     current_location: this.currentLocation,
-    //     bio: this.bio,
-    //     password: this.password,
-    //     password_confirmation: this.passwordConfirmation,
-    //   };
-    //   axios
-    //     .post("/api/users", params)
-    //     .then((response) => {
-    //       console.log(response.data);
-    //       this.$router.push("/login");
-    //     })
-    //     .catch((error) => {
-    //       this.errors = error.response.data.errors;
-    //     });
-    // },
+    submit: function () {
+      var params = {
+        //  -
+      };
+      axios
+        .post("/api/users", params)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/login");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
   },
 };
 </script>
