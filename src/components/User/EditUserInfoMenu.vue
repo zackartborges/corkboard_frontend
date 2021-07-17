e006fa6e7de62325791aca6629bbed30ace9c9f9
Zack Borges <zackartborges@gmail.com>
2021-07-17 19:24:57 -0400

    adds User Show page and edit user info button

---
+++ src/components/User/EditUserInfoMenu.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="edit-user-info-menu">
+    <button v-on:click="showUserInfo(user)">Edit</button>
+    <span class="edit-user-info">
+      <dialog id="user-details">
+        <form method="dialog">
+          <h2>Gear Info</h2>
+          <p>
+            Name:
+            <input type="text" v-model="user.name" />
+          </p>
+          <p>
+            Username:
+            <input type="text" v-model="user.username" />
+          </p>
+          <p>
+            Email:
+            <input type="text" v-model="user.email" />
+          </p>
+          <p>
+            Bio:
+            <input type="text" v-model="user.bio" />
+          </p>
+          <p>
+            Profile Picture:
+            <input type="text" v-model="user.profilePicture" />
+          </p>
+          <button v-on:click.prevent="updateProfileInfo(user)" data-dismiss="modal">Update</button>
+          <button>Close</button>
+        </form>
+      </dialog>
+    </span>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      user: {},
+      name: "",
+      username: "",
+      email: "",
+      bio: "",
+      profilePicture: "",
+      errors: [],
+    };
+  },
+  created: function () {
+    this.showUser();
+  },
+  methods: {
+    showUser: function () {
+      axios.get(`/api/users/${localStorage.getItem("user_id")}`).then((response) => {
+        // console.log(this.$route.params.id);
+        this.user = response.data;
+        console.log(response.data);
+      });
+    },
+    updateProfileInfo: function (user) {
+      var params = {
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+      };
+      axios.patch("api/users/" + user.id, params).then((response) => {
+        console.log("Updated Data", response.data);
+      });
+    },
+    showUserInfo: function (user) {
+      console.log(user);
+      this.currentUser = user;
+      document.querySelector("#user-details").showModal();
+    },
+  },
+};
+</script>
--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="users-show">
     <users-show-info></users-show-info>
+    <edit-user-info-menu></edit-user-info-menu>
   </div>
 </template>
 
 <script>
-import UsersShowInfo from "../components/UsersShowInfo.vue";
+import UsersShowInfo from "../components/User/UsersShowInfo.vue";
+import EditUserInfoMenu from "../components/User/EditUserInfoMenu.vue";
 // @ is an alias to /src
 
 export default {
   name: "UsersShow",
   components: {
     UsersShowInfo,
+    EditUserInfoMenu,
   },
 };
 </script>
