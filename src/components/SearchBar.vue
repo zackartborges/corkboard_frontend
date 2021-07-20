d0057c396778cf9d0cdd8254415cd2fcda103e3e
Zack Borges <zackartborges@gmail.com>
2021-07-20 17:06:50 -0400

    creates search bar in connectionsIndex

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,1 +1,25 @@
-<template></template>
+<template>
+  <div class="searchbar">
+    Search for Users:
+    <input v-model="titleFilter" list="titles" />
+    <datalist id="users">
+      <input v-model="titleFilter" />
+      <option v-for="user in filterBy(users, userFilter, 'username')" :key="user.id">{{ user.username }}</option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+import Vue2Filters from "vue2-filters";
+
+export default {
+  mixins: [Vue2Filters.mixin],
+  data: function () {
+    return {
+      userFilter: "",
+    };
+  },
+
+  methods: {},
+};
+</script>
--- src/components/UserConnections.vue
+++ src/components/UserConnections.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="connection-index">
+    <div class="searchbar">
+      Search for Users:
+      <input v-model="userFilter" list="users" />
+      <datalist id="users">
+        <input v-model="userFilter" />
+        <option v-for="user in filterBy(users, userFilter, 'username')" :key="user.id">
+          {{ user.username }}
+        </option>
+      </datalist>
+    </div>
     <div class="user-info">
       <h1>Your Accepted Connections</h1>
       <div v-for="connection in accepted_connections" v-bind:key="connection.id">
@@ -27,18 +37,27 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
+// import SearchBar from "../components/SearchBar.vue";
 
 export default {
+  mixins: [Vue2Filters.mixin],
+  components: {
+    // SearchBar,
+  },
   data: function () {
     return {
       user: {},
       connections: [],
       accepted_connections: [],
       pending_connections: [],
+      userFilter: "",
+      users: [],
     };
   },
   created: function () {
     this.connectionIndex();
+    this.usersIndex();
   },
   methods: {
     // showUser: function () {
@@ -48,6 +67,13 @@ export default {
     //     console.log(response.data);
     //   });
     // },
+    usersIndex: function () {
+      axios.get(`/api/users`).then((response) => {
+        this.users = response.data;
+        console.log("your users:", this.users);
+      });
+      // this.accepted_connections = this.connections.filter((connection) => connection[0]["status"] === 1);
+    },
     connectionIndex: function () {
       axios.get(`/api/connections`).then((response) => {
         this.connections = response.data;
