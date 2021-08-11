1db46c30d64dc72094a1d1c6da19e14114b06672
winstonbarbe <winston.barbe@gmail.com>
2021-08-11 12:53:51 -0400

    updates landing page of user after logging in

--- src/components/LoginInput.vue
+++ src/components/LoginInput.vue
@@ -40,11 +40,12 @@ export default {
       axios
         .post("/api/sessions", params)
         .then((response) => {
-          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
           localStorage.setItem("user_id", response.data.user_id);
           // this.$router.push("/");
-          this.$router.push("/");
+          this.$router.push("/connections");
         })
         .catch((error) => {
           console.log(error.response);
