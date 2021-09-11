4e96f9fa6c8f241f46d12d4c75ae64920eae5e0d
Zack Borges <zackartborges@gmail.com>
2021-09-11 08:20:46 -0400

    fixes some of the styling on home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="body">
+    <h1>Welcome to CorkBoard!</h1>
+    <h3 style="color: #64ffda">This Application is currently still in the development process.</h3>
+    <br />
+    <br />
+    <p>
+      CorkBoard is a simple demo of a peer to peer messaging app.
+      <br />
+      <br />
+    </p>
+  </v-container>
+</template>
+
+<script></script>
+
+<style scoped>
+.body {
+  display: flex;
+  /* flex-direction: column; */
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 100px 0 0 0;
+}
+</style>
